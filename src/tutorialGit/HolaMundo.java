90a5b1f358f060794368383ceced22c851fc4344
usuario <usuario@pc21-096>
2023-09-26 16:25:44 +0200

    Mi primer commit

---
+++ src/tutorialGit/HolaMundo.java
@@ -0,0 +1,12 @@
+package tutorialGit;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HOLA CMEPPS!");
+
+	}
+
+}
